e937a7528f109a1dcd968080bacf51a4e7dc4b8a
Matheus <roxx77344@gmail.com>
2019-06-06 17:24:13 -0300

    Test

--- calculator.py
+++ calculator.py
@@ -7,29 +7,28 @@ def cls():
 
 def calcularlsd(x = 0, n = 0):
 
-    x = int(input("Dose em ug: "))
-    n = int(input("Dias desde o uso: "))
+    x = int(input("Lsd dosage in ug: "))
+    n = int(input("Days since you've used lsd: "))
 
     dosagem = (x / 100) * 280.059565 * (math.pow(n,-0.41256595))
-    print("Você vai precisar usar {:.1f}ug para sentir os mesmos efeitos de {:.1f}ug".format(dosagem, x))
-    print("Voltando pra tela de inicio em 5 segundos")
+    print("You will need to take {:.1f}ug to feel the same effects as {:.1f}ug".format(dosagem, x))
+    print("Going back to home screen in 5 seconds")
     time.sleep(5)
     cls()
     main()
 
 def calcularmdma(x = 0, y = 0):
-    x = float(input("Digite seu peso: "))
+    x = float(input("Type your weight in kg: "))
     y = x * 1.5
-    print("Dosagem segura: {:.0f}mg".format(y))
-    print("Voltando pra tela de inicio em 5 segundos")
+    print("Safe dosage: {:.0f}mg".format(y))
+    print("Going back to home screen in 5 seconds")
     time.sleep(5)
     cls()
     main()
 
 
 def main():
-
-    escolha = input("1 - Calcular tolerancia de psicodélicos \n2 - Calcular dosagem segura de MDMA\n")
+    escolha = input("1 - Calculate lsd tolerance \n2 - Calculate a safe dosage of MDMA\n")
     if escolha == '1':
         calcularlsd()
     elif escolha == '2':
