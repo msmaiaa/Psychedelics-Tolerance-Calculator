86c7b1e9c0997e36dbfd432c83d348f07fb4ebc1
Matheus <roxx77344@gmail.com>
2019-06-06 17:42:32 -0300

    Changed some conditions in lsd tolerance

--- calculator.py
+++ calculator.py
@@ -2,6 +2,12 @@ import math
 import time
 import os
 
+def loop():
+    print("Going back to home screen in 10 seconds")
+    time.sleep(10)
+    cls()
+    main()
+
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
@@ -10,21 +16,19 @@ def calcularlsd(x = 0, n = 0):
     x = int(input("Lsd dosage in ug: "))
     n = int(input("Days since you've used lsd: "))
 
-    dosagem = (x / 100) * 280.059565 * (math.pow(n,-0.41256595))
-    print("You will need to take {:.1f}ug to feel the same effects as {:.1f}ug".format(dosagem, x))
-    print("Going back to home screen in 5 seconds")
-    time.sleep(5)
-    cls()
-    main()
+    if n < 0 or n > 12:
+        print("Days need to be above 0 and below 12 (when the tolerance is gone)")
+        loop()
+    else:
+        dosagem = (x / 100) * 280.059565 * (math.pow(n,-0.41256595))
+        print("You will need to take {:.0f}ug to feel the same effects as {:.1f}ug".format(dosagem, x))
+        loop()
 
 def calcularmdma(x = 0, y = 0):
     x = float(input("Type your weight in kg: "))
     y = x * 1.5
     print("Safe dosage: {:.0f}mg".format(y))
-    print("Going back to home screen in 5 seconds")
-    time.sleep(5)
-    cls()
-    main()
+    loop()
 
 
 def main():
